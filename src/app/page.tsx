3261a1d79bb56747192832e3626023417a3fbf5e
Marcos Gusttavo <marcosgusttavo2@gmail.com>
2026-07-12 17:49:01 -0300

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,29 @@ import AgendaCard from "@/components/agendacards";
 import Header from "@/components/header";
 import Decorations from "@/components/Decoration";
 import { agenda } from "@/data/agenda";
-import hero from "@/components/Hero";
+import AnimatedBackground from "@/components/AnimatedBackground";
 
 export default function Home() {
- 
   return (
-    <main className="relative min-h-screen overflow-hidden bg-neutral-950 px-4 py-8 text-white">
+    <main className="relative min-h-screen overflow-hidden px-4 py-8 text-white">
+      <AnimatedBackground />
       <Decorations />
-      
 
       <section className="relative z-10 mx-auto w-full max-w-[320px] px-2 sm:max-w-[360px] md:max-w-[420px] lg:max-w-[460px]">
         <Header />
-        <hero />
-        
+
         <div className="mt-8 space-y-5">
           {agenda.map((show, index) => (
-  <AgendaCard
-    key={show.local}
-    dia={show.dia}
-    local={show.local}
-    horario={show.horario}
-    imagem={show.imagem}
-    maps={show.maps}
-    delay={index * 0.2}
-  />
-))}
-          
+            <AgendaCard
+              key={`${show.dia}-${show.local}`}
+              dia={show.dia}
+              local={show.local}
+              horario={show.horario}
+              imagem={show.imagem}
+              maps={show.maps}
+              delay={index * 0.2}
+            />
+          ))}
         </div>
       </section>
     </main>
